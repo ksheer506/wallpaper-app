f5133e5dc3cbfec624da9047958d9af4283453ca
ksheer506 <ksheer506@naver.com>
2022-08-01 18:07:44 +0900

    헤더 스타일링

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
+import styled from "styled-components"
+
+const StyledHead = styled.header`
+  width: 100vw;
+  height: 60px;
+  display: flex;
+  position: fixed;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  box-shadow: 0px 3px 10px #e6e6e6;
+  margin-bottom: 15px;
+`
+
 export default function Header() {
-  return(
-  <header>
-    <p>배경화면</p>
-  </header>)
+  return (
+    <StyledHead>
+      <h1>배경화면</h1>
+    </StyledHead>
+  );
 }
