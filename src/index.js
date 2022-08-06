f0a0fedd9bc0704125b89bf7a5afe8c574055a43
ksheer506 <ksheer506@naver.com>
2022-08-06 20:52:35 +0900

    Modal 적용

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Modal } from './components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Modal width="60%" height="65%"><App /></Modal>
   </React.StrictMode>
 );
