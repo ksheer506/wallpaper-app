e5caa438688ddded88c618a5ae70be71f64b25a5
ksheer506 <ksheer506@naver.com>
2022-08-13 20:03:38 +0900

    무한 스크롤 구현, AbortController 도입

--- src/App.js
+++ src/App.js
@@ -1,45 +1,93 @@
 import './App.css';
-import Skeleton from './stories/Skeleton';
-import ImgContainer from './stories/ImageContainer';
+import Image from './components/Image/ImageContainer';
 import Header from './components/Header';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Loading from './stories/Loading';
+import ImageSkel from './components/Image/ImageSkel';
+
+const N = 14;
 
 function App() {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState(new Array(N).fill(0).map((_, i) => i));
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
+  const id = useRef(0);
+  const bottom = useRef();
 
   useEffect(() => {
-    (async () => {
-      const req = await fetch(`https://api.pexels.com/v1/curated/?page=${currentPage}`, {
-        method: "GET",
-        headers: {
-          authorization: "563492ad6f91700001000001ea3a86ac3adb417dbe8fdfc16dfdbdc3"
-        }
-      })
+    const options = {
+      rootMargin: "500px"
+    }
+    const io = new IntersectionObserver(([e]) => {
+      if (e.isIntersecting) {
+        const dumArr = new Array(N).fill(0).map((_, i) => id.current + i);
 
-      if (req.ok) {
+        setCurrentPage(prev => prev + 1);
+        setImages((prev) => [...prev, ...dumArr]);
+        id.current += N;
+      }
+    }, options);
+
+    io.observe(bottom.current);
+
+    return () => io.disconnect();
+  }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    (async () => {
+      await new Promise((resolve) =>
+        setTimeout(resolve, 1000)
+      )
+
+      try {
+        // "http://localhost:3001/blogs"
+        const req = await fetch(`https://api.pexels.com/v1/curated/?page=${currentPage}`, {
+          method: "GET",
+          headers: {
+            authorization: "563492ad6f91700001000001ea3a86ac3adb417dbe8fdfc16dfdbdc3"
+          },
+          signal: controller.signal
+        })
         const res = await req.json();
 
-        setImages(res.photos);
-        setIsLoading(false);
+        setImages(prev => {
+          const sliced = prev.filter((e) => typeof e !== "number");
+
+          return [...sliced, ...res.photos]
+        });
+        // setIsLoading(false);
+      }
+      catch (err) {
+        if (controller.signal.aborted) {
+          console.log("요청 취소됨");
+        } else {
+          console.log("요청 실패");
+        }
       }
     })();
 
-  }, [])
+    /* return () => controller.abort(); */
+  }, [currentPage])
 
   return (
     <>
       <Header />
       <main>
-        {isLoading ? null :
-          images.map((pic) => <ImgContainer {...pic} width={200} height={200} key={pic.id} />)
-        }
+        {images.map((pic) => <Image {...pic} width="auto" height="auto" key={pic.id || pic} />)}
       </main>
-
-
+      <div ref={bottom}></div>
+      {/* <Modal width="600px" height="400px" /> */}
     </>
   );
 }
 
 export default App;
+
+
+/* 
+{isLoading ? images.map((_, i) => <ImageSkel key={i} />) :
+          images.map((pic) => <Image {...pic} width={"auto"} height={"auto"} setOpenModal={setOpenModal} key={pic.id} />)
+        }
+         */
