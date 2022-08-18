83fbb9b90c1b1772d36bab82554f7fe356c16f55
ksheer506 <ksheer506@naver.com>
2022-08-18 18:45:08 +0900

    컨트롤러 추가

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,6 @@
 module.exports = {
-  login: require("./controller/login")
+  login: require("./controller/login"),
+  getComments: require("./controller/comments/getComments"),
+  addComments: require("./controller/comments/addComment"),
+  editComments: require("./controller/comments/editComment")
 }
