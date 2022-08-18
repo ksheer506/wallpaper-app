ab89c76c935b5497cf2506a257ea8a0add92b090
ksheer506 <ksheer506@naver.com>
2022-08-18 20:55:09 +0900

    GET 엔드포인트 작성

--- server/controller/comments/getComments.js
+++ server/controller/comments/getComments.js
@@ -1,5 +1,11 @@
+const { fetchDB } = require("../util/accessDB");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { picID } = req.params;
+  const path = `${process.cwd()}/server/db/comments/${picID}.json`;
+  const comments = await fetchDB(path);
 
+  console.log(comments);
+
+  res.status(200).send(comments);
 }
